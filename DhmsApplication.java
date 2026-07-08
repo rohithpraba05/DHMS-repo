e9bd8c227d35d2747bdc538a758c711632c61623
rohithpraba05 <rohithpraba71@gmail.com>
2026-07-08 12:47:15 +0530

    Add files via upload

---
+++ DhmsApplication.java
@@ -0,0 +1,15 @@
+package com.dhms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main application class for Decentralized Hospital Management System
+ */
+@SpringBootApplication
+public class DhmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DhmsApplication.class, args);
+    }
+}
